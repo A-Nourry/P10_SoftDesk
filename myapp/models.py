fbdfc17b2725018310762bd1eea107c05ba933ce
Andrew Nourry <andrewnourry@gmail.com>
2022-09-26 21:15:54 +0200

    add models:
    -user
    -contributors
    -projects
    -issues
    -comments

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Contributors(models.Model):
+    user_id = models.IntegerField
+    project_id = models.IntegerField
+    permission = models.Choices
+    role = models.CharField(max_length=128, verbose_name="rôle")
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=128, verbose_name="titre")
+    desciption = models.CharField(max_length=1000, verbose_name="description")
+    type = models.CharField(max_length=128, verbose_name="type")
+    author_user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+
+class Issues(models.Model):
+    title = models.CharField(max_length=128, verbose_name="titre")
+    desciption = models.CharField(max_length=1000, verbose_name="description")
+    tag = models.CharField(max_length=128, verbose_name="balise")
+    priority = models.CharField(max_length=128, verbose_name="priorité")
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    status = models.CharField(max_length=128, verbose_name="status")
+    author_user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    assignee_user_id = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="assignee_user_id",
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    desciption = models.CharField(max_length=1000, verbose_name="description")
+    author_user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    issue_id = models.ForeignKey(Issues, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
