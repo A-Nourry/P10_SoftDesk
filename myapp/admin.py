58edd9892ae0b553f6a76f89d81ba544ea148d27
Andrew Nourry <andrewnourry@gmail.com>
2022-09-27 17:38:11 +0200

    added all myapp models classes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Projects, Contributors, Issues, Comments
 
-# Register your models here.
+
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "type", "author_user_id")
+
+
+class ContributorsAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "project_id", "permission", "role")
+
+
+class IssuesAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "description",
+        "tag",
+        "priority",
+        "project_id",
+        "status",
+        "author_user_id",
+        "assignee_user_id",
+        "created_time",
+    )
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("id", "description", "author_user_id", "issue_id", "created_time")
+
+
+admin.site.register(Projects, ProjectsAdmin)
+admin.site.register(Contributors, ContributorsAdmin)
+admin.site.register(Issues, IssuesAdmin)
+admin.site.register(Comments, CommentsAdmin)
