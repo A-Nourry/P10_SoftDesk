5d3ec5eebc1985662ea23219c97f1d53ee700813
Andrew Nourry <andrewnourry@gmail.com>
2022-09-28 19:34:31 +0200

    active user set to True but default

--- authentication/models.py
+++ authentication/models.py
@@ -48,7 +48,7 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
