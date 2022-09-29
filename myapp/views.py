7b9b139eb5474ac6d4b237a3523f6895369d604c
Andrew Nourry <andrewnourry@gmail.com>
2022-09-30 01:32:18 +0200

    contributors and issues views added

--- myapp/views.py
+++ myapp/views.py
@@ -5,8 +5,8 @@ from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 
-from .models import Projects
-from .serializers import ProjectsSerializer
+from .models import Projects, Contributors, Issues
+from .serializers import ProjectsSerializer, ContributorsSerializer, IssuesSerializer
 
 User = get_user_model()
 
@@ -74,9 +74,11 @@ class DetailProjectView(MultipleSerializerMixin, APIView):
 
         project = Projects.objects.get(id=project_id)
 
+        current_user = User.objects.get(id=request.user.id)
+
         serializer = self.serializer_class(project, data=data)
 
-        if serializer.is_valid():
+        if serializer.is_valid() and project.author_user_id == current_user:
             serializer.save()
 
             response = {
@@ -85,6 +87,11 @@ class DetailProjectView(MultipleSerializerMixin, APIView):
             }
 
             return Response(data=response, status=status.HTTP_201_CREATED)
+
+        else:
+            response = {
+                "message": "Vous n'êtes pas l'auteur de ce projet : action non autorisé !",
+            }
 
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -103,6 +110,168 @@ class DetailProjectView(MultipleSerializerMixin, APIView):
         else:
             response = {
                 "message": "Vous n'êtes pas l'auteur de ce projet : action non autorisé !",
+            }
+
+        return Response(data=response, status=status.HTTP_201_CREATED)
+
+
+class ContributorsView(MultipleSerializerMixin, APIView):
+
+    """permission_classes = [IsAuthenticated]"""
+
+    serializer_class = ContributorsSerializer
+
+    def get(self, request, project_id, *args, **kwargs):
+
+        contributors = Contributors.objects.filter(project_id=project_id)
+
+        serializer = self.serializer_class(contributors, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, project_id):
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+
+        project = Projects.objects.get(id=project_id)
+
+        contributors = Contributors.objects.filter(
+            project_id=project, user_id=data["user_id"]
+        )
+
+        if (
+            serializer.is_valid() and len(contributors) < 1
+        ):  # verify if contributors doesn't already exist
+            serializer.save(project_id=project)
+
+            response = {
+                "message": "Contributeur ajouté avec succès !",
+                "data": serializer.data,
+            }
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        else:
+            response = {
+                "message": "ce contributeur à déjà été ajouté !",
+            }
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserContributorsView(MultipleSerializerMixin, APIView):
+
+    """permission_classes = [IsAuthenticated]"""
+
+    serializer_class = ContributorsSerializer
+
+    def delete(self, request, project_id, user_id):
+
+        project = Projects.objects.get(id=project_id)
+
+        user = User.objects.get(id=user_id)
+
+        current_user = User.objects.get(id=request.user.id)
+
+        contributor = Contributors.objects.get(project_id=project, user_id=user)
+
+        if project.author_user_id == current_user:
+            contributor.delete()
+
+            response = {
+                "message": "contributeur supprimé du projet avec succès !",
+            }
+        else:
+            response = {
+                "message": "Vous n'êtes pas l'auteur de ce projet : action non autorisé !",
+            }
+
+        return Response(data=response, status=status.HTTP_201_CREATED)
+
+
+class ProjectIssueView(MultipleSerializerMixin, APIView):
+
+    """permission_classes = [IsAuthenticated]"""
+
+    serializer_class = IssuesSerializer
+
+    def get(self, request, project_id, *args, **kwargs):
+
+        issue = Issues.objects.filter(project_id=project_id)
+
+        serializer = self.serializer_class(issue, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, project_id):
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+
+        project = Projects.objects.get(id=project_id)
+
+        current_user = User.objects.get(id=request.user.id)
+
+        if serializer.is_valid():
+            serializer.save(project_id=project, author_user_id=current_user)
+
+            response = {
+                "message": "Problème créé avec succès !",
+                "data": serializer.data,
+            }
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IssueView(MultipleSerializerMixin, APIView):
+
+    """permission_classes = [IsAuthenticated]"""
+
+    serializer_class = IssuesSerializer
+
+    def put(self, request, project_id, issue_id):
+        data = request.data
+
+        issue = Issues.objects.get(id=issue_id, project_id=project_id)
+
+        current_user = User.objects.get(id=request.user.id)
+
+        serializer = self.serializer_class(issue, data=data)
+
+        if serializer.is_valid() and issue.author_user_id == current_user:
+            serializer.save()
+
+            response = {
+                "message": "Problème modifié avec succès !",
+                "data": serializer.data,
+            }
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        else:
+            response = {
+                "message": "Vous n'êtes pas l'auteur de ce problème : action non autorisé !",
+            }
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, project_id, issue_id):
+
+        issue = Issues.objects.get(id=issue_id, project_id=project_id)
+
+        current_user = User.objects.get(id=request.user.id)
+
+        if issue.author_user_id == current_user:
+            issue.delete()
+
+            response = {
+                "message": "Problème supprimé avec succès !",
+            }
+        else:
+            response = {
+                "message": "Vous n'êtes pas l'auteur de ce problème: action non autorisé !",
             }
 
         return Response(data=response, status=status.HTTP_201_CREATED)
